852003659ada91310d8d1586992dae6595565804
mrodrigues1 <i&@gqgvZVsnlUxeMX1X1rqyzBUOI7^LpkoM55aic3lENC!nQXq>
2026-04-08 13:48:25 -0300

    fix(signing): use environment variables for keystore path and manage workspace in CI
    - Updated `app/build.gradle.kts` to dynamically resolve the keystore path using `System.getenv`.
    - Modified the release workflow to decode the keystore file into the GitHub workspace and configure the `SIGNING_KEYSTORE` environment variable.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("release.keystore")
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "release.keystore")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD") ?: ""
             keyAlias = System.getenv("SIGNING_KEY_ALIAS") ?: ""
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD") ?: ""
